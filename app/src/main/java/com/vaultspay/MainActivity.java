0e30de21eabb89f0759f52269b44fb7d1bcb7f2d
naveenkumarsangwan <naveenkumarsangwan@gmail.com>
2020-08-17 16:40:14 +0530

    Initial commit

---
+++ app/src/main/java/com/vaultspay/MainActivity.java
@@ -0,0 +1,26 @@
+package com.vaultspay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.valutspaylibrary.RssFeedProvider;
+import com.valutspaylibrary.RssItem;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void updateDetail(String uri) {  //
+        List<RssItem> list = RssFeedProvider
+                .parse(uri);
+        String itemListAsString = list.toString();
+        listener.onRssItemSelected(itemListAsString);
+    }
+}
